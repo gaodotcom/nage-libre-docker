bf3a92b1037b10fb289ad3595411aa63389af448
gaodotcom <loicgao@gmail.com>
2026-05-23 11:33:54 +0200

    mise à jour, mysql8 notamment

--- config/mariadb/init.sql
+++ config/mariadb/init.sql
@@ -1,10 +1,10 @@
 -- Drupal 7 DB
--- CREATE DATABASE IF NOT EXISTS nage-libre CHARACTER SET utf8;
+-- CREATE DATABASE IF NOT EXISTS nage-libre CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 -- CREATE USER IF NOT EXISTS 'nage-libre_usr'@'%' IDENTIFIED BY 'nage-libre';
 -- GRANT ALL PRIVILEGES ON nage-libre.* TO 'nage-libre'@'%';
 
 -- Drupal 10 DB
--- CREATE DATABASE IF NOT EXISTS nagelibre10 CHARACTER SET utf8;
+-- CREATE DATABASE IF NOT EXISTS nagelibre10 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 -- CREATE USER IF NOT EXISTS 'nagelibre10_usr'@'%' IDENTIFIED BY 'nagelibre10';
 -- GRANT ALL PRIVILEGES ON nagelibre10.* TO 'nagelibre10'@'%';
 
